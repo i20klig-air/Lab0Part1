eb85ef0e19d5eecee21934c49bae74b8a48f7ebb
i20klig-air <i20klig@siena.edu>
2026-09-14 11:10:04 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,19 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author Ilan Kligerman
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Albany NY");
+        System.out.println("Ya Hala");
+        System.out.println("Steak Tacos");
+        
+        
     }
+    
+    
 }
